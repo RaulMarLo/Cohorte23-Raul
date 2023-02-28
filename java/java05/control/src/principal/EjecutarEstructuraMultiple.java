94518a79719fc10d7805a2ace0f0b161c919bdd3
Raul <mtzraul2808@gmail.com>
2023-02-28 17:36:52 -0600

    java05

---
+++ java/java05/control/src/principal/EjecutarEstructuraMultiple.java
@@ -0,0 +1,17 @@
+package principal;
+
+import control.EstructuraIf;
+import control.EstructuraMultiple;
+
+public class EjecutarEstructuraMultiple {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		EstructuraMultiple ei = new EstructuraMultiple();
+		ei.vocal();
+		ei.notas();
+		ei.ternario();
+
+	}
+
+}
