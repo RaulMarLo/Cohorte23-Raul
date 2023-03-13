f80ce67d8e36382e46f67ef5440c98f3455380dd
Raul <mtzraul2808@gmail.com>
2023-03-13 17:16:27 -0600

    Java 12

---
+++ liveCoding/LC.js
@@ -0,0 +1,25 @@
+const myArray = ['insecto', 'bootcamp', 'mangos', 'reptil', 'mosca', 'escritorio'];
+
+
+const myString = 'Brandon'
+
+
+function CompararNumCaracteres (a, b){
+    
+    let arrayVacio = [];
+
+
+    for(let i = 0; i < a.length; i++){
+        if(a[i].length > b.length){
+
+
+        arrayVacio.push(a[i]);
+        
+    }
+    
+}
+return arrayVacio;
+}
+
+let Resultado = (CompararNumCaracteres(myArray,myString));
+console.log(Resultado);
